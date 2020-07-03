c91d5c6fd6bd99632cff3d42c8f9da5842efb492
Khdbble <fb503d52.af1c4d@gmail.com>
2020-07-03 10:29:03 +0900

    made writes as volatile to avoid erroneous optimization

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -43,9 +43,12 @@ struct ScreenChar {
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
+
+use volatile::Volatile;
+
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 pub struct Writer {
@@ -67,10 +70,10 @@ impl Writer {
                 let col = self.column_position;
 
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
-                    color_code,
-                };
+                    color_code: color_code,
+                });
                 self.column_position += 1;
             }
         }
