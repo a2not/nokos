3f18bb18fce0d9f3138995903c903a71550d8f6a
Khdbble <fb503d52.af1c4d@gmail.com>
2020-07-09 16:03:02 +0900

    loaded the IDT
    with this the CPU now uses our new interrupt descriptor table.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,9 +1,17 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
 
 pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
