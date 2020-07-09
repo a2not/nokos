ccf8ae2e6626080e015aaae2b910e879c39d1219
Khdbble <fb503d52.af1c4d@gmail.com>
2020-07-09 16:24:32 +0900

    added test case for exception handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -20,4 +20,8 @@ extern "x86-interrupt" fn breakpoint_handler(
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
-
+#[test_case]
+fn test_breakpoint_exception() {
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+}
