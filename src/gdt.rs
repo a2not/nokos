326755c472588cdff9695bda5ee367763f4f0452
Khdbble <fb503d52.af1c4d@gmail.com>
2020-07-12 13:04:52 +0900

    created a GDT which holds TSS static

--- src/gdt.rs
+++ src/gdt.rs
@@ -19,3 +19,14 @@ lazy_static! {
     };
 }
 
+use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor};
+
+lazy_static! {
+    static ref GDT: GlobalDescriptorTable = {
+        let mut gdt = GlobalDescriptorTable::new();
+        gdt.add_entry(Descriptor::kernel_code_segment());
+        gdt.add_entry(Descriptor::tss_segment(&TSS));
+        gdt
+    };
+}
+
