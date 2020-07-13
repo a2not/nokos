94d83fd8c8eb854423746f9a72fdd00ba9b184ba
Khdbble <fb503d52.af1c4d@gmail.com>
2020-07-13 13:55:53 +0900

    initial configuration of Intel 8259 PIC

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -39,3 +39,12 @@ extern "x86-interrupt" fn double_fault_handler(
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}",stack_frame);
 }
 
+use pic8259_simple::ChainedPics;
+use spin;
+
+pub const PIC_1_OFFSET: u8 = 32;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub static PICS: spin::Mutex<ChainedPics> =
+    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+
--- src/lib.rs
+++ src/lib.rs
@@ -76,5 +76,6 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
 }
 
