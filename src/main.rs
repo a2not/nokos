4c1d930bf4926af57d5e8761005d11404a16dfa3
Khdbble <fb503d52.af1c4d@gmail.com>
2020-07-23 19:30:50 +0900

    initialization of kernel heap

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,21 @@ use alloc::boxed::Box;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::{structures::paging::Page, VirtAddr};
+    use x86_64::VirtAddr;
+    use nokos::allocator;
     use nokos::memory::{self, BootInfoFrameAllocator};
-
+    
     println!("Hello World{}", "!");
     nokos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-
     let mut frame_allocator = unsafe{
         BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
 
-    // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    // write the string `New!` to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
     
     let x = Box::new(41);
 
